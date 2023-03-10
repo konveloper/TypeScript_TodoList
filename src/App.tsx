c1571398e5c8618fb0b922689685d1217ae1ce91
konveloper <konveloper@gmail.com>
2023-03-10 17:54:56 +0900

    docs : 폴더 경로 수정

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='mainContainer'>
+      <div className='appContainer'>
+        <input
+          type='text'
+          className='inputText'
+          placeholder='내용을 입력하세요.'
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
