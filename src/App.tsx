9a601777bb26b0f98bb68a11581a11518b310118
konveloper <konveloper@gmail.com>
2023-03-25 12:58:28 +0900

    feat : 체크박스 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
-import Item from './components/Item';
+import TodoList from 'components/TodoList';
 
 function App() {
   return (
     <div className='mainContainer'>
       <div className='appContainer'>
-        <Item text='테스트' />
-        <Item text='테스트 테스트' />
-        <Item
-          text='테스트 테스트 테스트 테스트 테스트 테스트 테스트 테스트 테스트 '
-          completed
-        />
+        <TodoList />
         <input
           type='text'
           className='inputText'
